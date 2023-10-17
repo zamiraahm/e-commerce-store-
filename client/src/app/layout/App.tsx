d68081efcba8d644891266df20ed36cc0c346213
Zamira Ahmeti <zamiraahm1@gmail.com>
2023-10-18 00:48:43 +0200

    app initialisation

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,29 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agents";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const buyerId = getCookie('buyerId');
+    if(buyerId){
+      agent.Basket.get()
+       .then(basket => setBasket(basket))
+       .catch(error => console.log(error))
+       .finally(() => setLoading(false));
+    }
+  }, [setBasket])
+
+
   const [darkMode,setDarkMode]=useState(false); 
   const paletteType = darkMode ? 'dark' : 'light';
   const theme=createTheme({
@@ -20,6 +38,8 @@ function App() {
   function handleThemeChange(){
     setDarkMode(!darkMode);
   }
+
+  if(loading) return <LoadingComponent message="Initialising app..."/>
 
   return (
     <ThemeProvider theme={theme}>
