6b3aa02f9f4e9420c63aeca262e2a4f081081013
Zamira Ahmeti <zamiraahm1@gmail.com>
2023-10-19 00:41:12 +0200

    basket summary

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -5,6 +5,7 @@ import agent from "../../app/api/agents";
 import { LoadingButton } from "@mui/lab";
 import { useState } from "react";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/util/util";
 
 interface Props{
     product:Product;
@@ -41,7 +42,7 @@ export default function ProductCard({product}:Props){
       />
       <CardContent>
         <Typography gutterBottom color='secondary' variant="h5">
-         ${(product.price /100).toFixed(2)}
+         {currencyFormat(product.price)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
                 {product.author} / {product.genre}
