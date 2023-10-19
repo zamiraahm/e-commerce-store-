24af3a33cb86387beb8c4cefcf6be056f121a8d1
Zamira Ahmeti <zamiraahm1@gmail.com>
2023-10-19 18:13:36 +0200

    search functionality to the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,9 +18,15 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy,
+          string searchTerm)
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+                  .Sort(orderBy)
+                  .Search(searchTerm)
+                  .AsQueryable();
+
+            return await query.ToListAsync();
         }
 
         [HttpGet("{id}")]
---
+++ API/Extensions/ProductExtensions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Extensions
+{
+    public  static class ProductExtensions
+    {
+        public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
+        {
+            if(string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p => p.Name);
+            query = orderBy switch
+            {
+                "price" => query.OrderBy(p => p.Price),
+                "priceDesc" => query.OrderByDescending(p => p.Price),
+                _ => query.OrderBy(p => p.Name)
+            };
+             return query;
+        }
+
+        public static IQueryable<Product> Search(this IQueryable<Product> query, string searchTerm)
+        {
+            if(string.IsNullOrEmpty(searchTerm)) return query;
+
+            var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
+
+            return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
+        }
+    }
+}
