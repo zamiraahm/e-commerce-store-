63427a75a59d6e2fe3a443e37dfce441a0a3b0fb
Zamira Ahmeti <zamiraahm1@gmail.com>
2023-10-26 21:20:55 +0200

    serving the client app from the API

--- API/Program.cs
+++ API/Program.cs
@@ -78,6 +78,8 @@ if (app.Environment.IsDevelopment())
         c.ConfigObject.AdditionalItems.Add("persistAuthorization","true");
     });
 } 
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseCors(opt=>
 {
@@ -88,6 +90,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToController("Index","Fallback");
 
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
