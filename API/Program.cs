b2bf614ac3a84d83e26bfdd40375ecdf66f8959f
lizableta <bletaeliza@gmail.com>
2023-10-27 15:47:19 +0200

    github connection

--- API/Program.cs
+++ API/Program.cs
@@ -39,10 +39,33 @@ builder.Services.AddSwaggerGen(c=> {
         }
     }); 
 });
-builder.Services.AddDbContext<StoreContext>(opt => 
+string connString;
+if (builder.Environment.IsDevelopment())
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else
 {
-   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    // Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+
+
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseNpgsql(connString);
 });
+
 builder.Services.AddCors();
 builder.Services.AddIdentityCore<User>(opt=>{
     opt.User.RequireUniqueEmail=true;
