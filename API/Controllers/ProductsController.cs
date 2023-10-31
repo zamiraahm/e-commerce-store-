84fe197ef222aa0bc87e4cc8cfe4b881ac221056
Zamira Ahmeti <zamiraahm1@gmail.com>
2023-10-31 20:17:12 +0100

    product endpoint

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 using System.Text.Json;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.RequestHelpers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -36,7 +38,7 @@ namespace API.Controllers
             return products;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetProduct")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
            var product = await _context.Products.FindAsync(id);
@@ -53,6 +55,19 @@ namespace API.Controllers
             var genres = await _context.Products.Select(p => p.Genre).Distinct().ToListAsync();
 
             return Ok(new {authors , genres});
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(CreateProductDto productDto)
+        {
+            _context.Products.Add(productDto);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if(result) return CreatedAtRoute("GetProduct", new {Id = product.Id}, product);
+
+            return BadRequest(new ProblemDetails {Title = "Problem creating new product"});
         }
     }
 }
