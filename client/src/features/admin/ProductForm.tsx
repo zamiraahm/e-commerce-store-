6714779303fad5424d4fa39cbb80ced515e68226
Zamira Ahmeti <zamiraahm1@gmail.com>
2023-11-06 00:34:02 +0100

    dropbox

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,8 +1,11 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
+import useProducts from "../../app/hooks/useProducts";
+import AppSelectList from "../../app/components/AppSelectList";
+import AppDropzone from "../../app/components/AppDropzone";
 
 interface Props {
     product?: Product;
@@ -10,44 +13,60 @@ interface Props {
 }
 
 export default function ProductForm({product, cancelEdit}: Props) {
-    const { control, reset } = useForm();
+    const { control, reset, handleSubmit , watch} = useForm();
+    const {authors, genres} = useProducts();
+    const watchFile = watch('file', null);
 
     useEffect(() => {
         if(product) reset(product);
     }, [product, reset])
+
+    function handleSubmitData(data: FieldValues){
+        console.log(data);
+    }
 
     return (
         <Box component={Paper} sx={{p: 4}}>
             <Typography variant="h4" gutterBottom sx={{mb: 4}}>
                 Product Details
             </Typography>
+            <form onSubmit={handleSubmit(handleSubmitData)} >
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={12}>
                     <AppTextInput control={control} name='name' label='Product name' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='author' label='Brand' />
+                    <AppSelectList control={control} items={authors} name='author' label='Author' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='genre' label='Type' />
+                    <AppSelectList control={control} items={genres} name='genre' label='Genre' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='price' label='Price' />
+                    <AppTextInput type='number' control={control} name='price' label='Price' />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='quantityInStock' label='Quantity in Stock' />
+                    <AppTextInput type='number' control={control} name='quantityInStock' label='Quantity in Stock' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='description' label='Description' />
+                    <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='pictureUrl' label='Image' />
+                    <Box display='flex' justifyContent='space-between' alignItems='center'>
+                    <AppDropzone control={control} name='file' />
+                    {watchFile ? (
+                        <img src={watchFile.preview} alt="preview" style={{maxHeight: 200}}/>
+                    ): (
+                        <img src={product?.pictureUrl} alt={product?.name} style={{maxHeight: 200}}/>
+
+                    )}
+                    </Box>
                 </Grid>
             </Grid>
             <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
                 <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
-                <Button variant='contained' color='success'>Submit</Button>
+                <Button type='submit' variant='contained' color='success'>Submit</Button>
             </Box>
+            </form>
         </Box>
     )
 }
